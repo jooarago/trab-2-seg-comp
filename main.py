340d8ce98f6c21afe3f956bd5898bfd7dabe25b3
fperone <148684728+fperone@users.noreply.github.com>
2025-06-23 21:55:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import hash
 import json
 
 enc_keys = []
-def teste_rsa_oaep_txt(filepath):
+def gerador_de_assinatura(filepath):
     # getting file content
     with open(filepath, 'rb') as f:
         msg_in_bytes = f.read()
@@ -24,9 +24,10 @@ def teste_rsa_oaep_txt(filepath):
     # decoding...
     print("Decifrando...")
     msg_decoded = oaep.decypher(msg_encoded, key_priv)
-    
+
     # showing results...
     print("Original:", msg_in_bytes.decode('utf-8', errors='replace'))
+    print("Cifrada:", hash.hash_base64(msg_encoded))
     print("Decifrada:", msg_decoded.decode('utf-8', errors='replace'))
     print("Sucesso???", msg_in_bytes == msg_decoded)
 
@@ -43,11 +44,11 @@ def teste_rsa_oaep_txt(filepath):
     with open("msg_assinada.json", "w") as f:
         json.dump(signed_data, f)
 
-def verificar_assinatura(filepath):
+def verificador_de_assinatura(filepath):
     # getting file content
     with open(filepath, 'r') as f:
         signed_data = json.load(f)
-        
+
     # getting signature and message
     mensagem_b64 = signed_data["mensagem_base64"]
     sig_b64 = signed_data["assinatura"]
@@ -55,13 +56,23 @@ def verificar_assinatura(filepath):
     # getting fixed-length hash from signature
     sig_bytes = hash.base64_to_bytes(sig_b64)
     hash_msg_original = oaep.decypher(sig_bytes, enc_keys[0][0]) # using public key to decrypt
-
+    print("Assinatura:", sig_b64)
     # getting fixed-length hash from message
     msg_bytes = hash.base64_to_bytes(mensagem_b64)
     msg_hash = hash.hash_sha3_256(msg_bytes)
 
-    # showing results...
-    print("Comparando assinaturas:", msg_hash == hash_msg_original)
+    print("Hash obtido pela Assinatura:", hash.hash_base64(msg_hash))
+    print("Hash obtido pelo conteúdo da mensagem:", hash.hash_base64(hash_msg_original))
 
-teste_rsa_oaep_txt("teste.txt")
-verificar_assinatura("msg_assinada.json")
+    # showing results...
+    print("Verificando assinatura:", msg_hash == hash_msg_original)
+
+while True:
+    x = input("Digite qualquer coisa para gerar uma assinatura,  ou 'sair' para sair: ")
+    if x == "sair":
+        break
+    else:
+        filepath = input("Digite o caminho do arquivo: ")
+        gerador_de_assinatura(filepath)
+        verificador_de_assinatura("msg_assinada.json")
+    
